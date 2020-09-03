058f021d7fef70d8de2f8c66c6215d8d0c061f99
adnank122 <adnank122@gmail.com>
2020-09-03 17:24:36 +0500

    :white_check_mark: Configured Jest and RNTL

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+const path = require("path");
+
+module.exports = {
+  preset: "react-native",
+  setupFilesAfterEnv: [
+    "@testing-library/jest-native/extend-expect",
+    "./jest.setup.js"
+  ],
+  clearMocks: true,
+  moduleDirectories: ["node_modules", path.join(__dirname, "src")],
+  setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/assetsTransformer.js",
+    "\\.(css|less)$": "<rootDir>/assetsTransformer.js"
+  }
+};

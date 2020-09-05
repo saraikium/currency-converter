fa4130e1d3235842139998456049b95d19db5285
adnank122 <adnank122@gmail.com>
2020-09-05 14:46:30 +0500

    Fixed the installation of RN Vector Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import "react-native-gesture-handler";
 import React from "react";
-import {} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 import {Home} from "./screens/Home";
+import {Options} from "./screens/Options";
+
+type MainStackParamsList = {
+  Home: undefined;
+  Options: undefined;
+};
+
+const MainStack = createStackNavigator<MainStackParamsList>();
+
+const MainStackScreen = () => (
+  <MainStack.Navigator initialRouteName="Options">
+    <MainStack.Screen name="Home" component={Home} />
+    <MainStack.Screen name="Options" component={Options} />
+  </MainStack.Navigator>
+);
 
 const App = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <MainStackScreen />
+    </NavigationContainer>
+  );
 };
 
 export default App;

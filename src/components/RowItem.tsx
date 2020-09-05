19fde7e364f5aa00bc9e40e0453b51449296c7ac
adnank122 <adnank122@gmail.com>
2020-09-05 14:45:05 +0500

    :sparkles: Added RowItem component

---
+++ src/components/RowItem.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {StyleSheet, TouchableOpacity, Text, View} from "react-native";
+
+import colors from "../constants/colors";
+
+const styles = StyleSheet.create({
+  row: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: colors.white
+  },
+  title: {
+    color: colors.text,
+    fontSize: 16
+  },
+  separator: {
+    backgroundColor: colors.border,
+    height: StyleSheet.hairlineWidth,
+    marginLeft: 20
+  }
+});
+
+interface Props {
+  title: string;
+  onPress(): void;
+  rightIcon: React.ReactElement;
+}
+
+export const RowItem = ({title, onPress, rightIcon}: Props) => (
+  <TouchableOpacity onPress={onPress} style={styles.row}>
+    <Text style={styles.title}>{title}</Text>
+    {rightIcon}
+  </TouchableOpacity>
+);
+
+export const RowSeparator = () => <View style={styles.separator} />;

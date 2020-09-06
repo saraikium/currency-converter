e22ab8b16a31382e597498fb0f2c9fed78ecc70f
adnank122 <adnank122@gmail.com>
2020-09-07 03:15:20 +0500

    :art: Update Logo component to use styled components

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,34 +1,32 @@
 import React from "react";
-import {View, Image, StyleSheet, Dimensions} from "react-native";
+import {Dimensions} from "react-native";
+import styled from "styled-components/native";
+
 import backgroundImage from "../assets/images/background.png";
 import logo from "../assets/images/logo.png";
 
 const screen = Dimensions.get("window");
 
-const styles = StyleSheet.create({
-  logoContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    position: "relative"
-  },
-  logoBackground: {
-    width: screen.width * 0.45,
-    height: screen.height * 0.45
-  },
-  logo: {
-    position: "absolute",
-    width: screen.width * 0.25,
-    height: screen.height * 0.25
-  }
-});
+const LogoContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const BackgroundImage = styled.Image`
+  width: ${screen.width * 0.4}px;
+  height: ${screen.height * 0.4}px;
+`;
+
+const FrontImage = styled.Image`
+  position: absolute;
+  width: ${screen.width * 0.2};
+  height: ${screen.height * 0.2};
+`;
 
 export const Logo = () => (
-  <View style={styles.logoContainer}>
-    <Image
-      source={backgroundImage}
-      style={styles.logoBackground}
-      resizeMode="contain"
-    />
-    <Image source={logo} style={styles.logo} resizeMode="contain" />
-  </View>
+  <LogoContainer>
+    <BackgroundImage source={backgroundImage} resizeMode="contain" />
+    <FrontImage source={logo} resizeMode="contain" />
+  </LogoContainer>
 );
